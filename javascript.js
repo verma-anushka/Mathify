5a7e179d7cbfbc5a82f34f23802dd8c200414682
Anushka Verma <v.anushka786@gmail.com>
2019-11-18 01:39:40 +0530

    Final changes

--- javascript.js
+++ javascript.js
@@ -1,33 +1,34 @@
 let body = document.getElementsByTagName("body")[0];
 
+// Variables
 var isPlay = false;
 var score;
-var rightAnswer;
-var rightAnswer1;
-var rightAnswer2;
 var types;
+var rightAnswer;
 var totalOptions = 4;
-var images = [ "q1", "q2", "q3", "q4", "q5", "q6"];
-let rightAnsPopUp = document.getElementById("rightAns");
-let wrongAnsPopUp = document.getElementById("wrongAns");
-var gameOverContainer = document.getElementById("gameOver");
-var allOptions = document.getElementsByClassName("option");
-        
+var images = [ "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "q16"]; // images
 let img = document.getElementById("question-img");
 img.innerHTML = '<img id="question-img" src="./images/q0.jpg" width="400" height="200">';
 
+// HTML elements
+let rightAnsPopUp = document.getElementById("rightAns");
+let wrongAnsPopUp = document.getElementById("wrongAns");
+var allOptions = document.getElementsByClassName("option");
+var scoreContainer = document.getElementById("score");
+var gameOverContainer = document.getElementById("gameOver");
+
+// Countdown
 var initialCount = 60,
  	count = initialCount,
-    timerPause = false;
+    timerPause = false,
+    timeInterval;
 var timerContainer = document.getElementById("timer");
 var value = document.getElementById("count");
 var pause = document.getElementById("pause");
 var lt = document.getElementById("timer-lt");
 var rt = document.getElementById("timer-rt");
 
-var scoreContainer = document.getElementById("score");
-var timeInterval;
-
+// Sounds
 // var ticking = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/231853/174721__drminky__watch-tick.wav');
 // var gameOverSound = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/231853/Elevator_Ding-SoundBible.com-685385892.mp3');
 var ticking = new Audio('sounds/tick-tock.mp3');
@@ -35,15 +36,43 @@ var correct = new Audio('sounds/correct.mp3');
 var incorrect = new Audio('sounds/incorrect.mp3');
 var gameOverSound = new Audio('sounds/gameOver.mp3');
 
+
+// Start Game
+document.getElementById("start").onclick = function(){
+
+    gameOverContainer.classList.add("no-show");
+    if (isPlay){
+        location.reload(); // Restart button - Reload page
+    }else{
+        isPlay = true;
+        score = 0;
+        document.getElementById("score-value").innerHTML = score;
+        document.getElementById("start").innerHTML = "Restart"; // Start -> Restart
+        scoreContainer.classList.remove("no-show");
+
+        // Countdown Counter
+        timerContainer.classList.remove("no-show");
+        ticking.play(); // Sound
+        timer();
+        timeInterval = setInterval(timer, 1000);
+        lt.style.animationPlayState = "paused";
+        rt.style.animationPlayState = "paused";
+
+        generateMCQs();
+    }
+
+}
+// Countdown
 function timer() {	    
 
     if (!timerPause) {
-        ticking.play();
+        ticking.play(); // Sound
+        
         body.style.opacity = "1";
         document.getElementById("pausedMsg").classList.add("no-show");
-
         document.getElementById("question").classList.remove("no-show");
-        document.getElementById("options").classList.remove("no-show");
+        document.getElementById("options").style.opacity = "1";
+
         lt.style.animationPlayState = "running";
         rt.style.animationPlayState = "running";
 	  	count = count - 1;
@@ -55,7 +84,6 @@ function timer() {
         value.innerHTML = count;
   	}else{
         document.getElementById("question").classList.add("no-show");
-        // document.getElementById("options").classList.add("no-show");
         document.getElementById("options").style.opacity = "0.05";
         document.getElementById("pausedMsg").classList.remove("no-show");
 
@@ -75,68 +103,34 @@ pause.addEventListener("click", function(){
 	}
 });
 
-document.getElementById("start").onclick = function(){
 
-    // Checking if the user is playing/not
-    if (isPlay){
-        // Restart button - Reload page
-        location.reload();
-    }else{
-        // Start button
-        isPlay = true;
+// Answer check
+for(var i=1; i<=totalOptions; i++){
 
-        // Initializing the value of score
-        score = 0;
-        document.getElementById("score-value").innerHTML = score;
-
-        // Start -> Restart
-        document.getElementById("start").innerHTML = "Restart";
-
-        // Countdown Counter
-        timerContainer.classList.remove("no-show");
-        ticking.play();
-        timer();
-        timeInterval = setInterval(timer, 1000);
-        lt.style.animationPlayState = "paused";
-        rt.style.animationPlayState = "paused";
-
-        scoreContainer.classList.remove("no-show");
-
-        // Generating MCQs
-        generateMCQs();
-    }
-
-}
-
-for(var i=1; i<=4; i++){
     document.getElementById("option" + i).onclick = function(){
-
         if(isPlay){
 
             // Right answer 
             if(this.innerHTML == rightAnswer){
-
-                console.log(rightAnswer);
-                correct.play();
-
+                correct.play(); // Sound
                 // Update score
-                score++;
+                score += 2; 
                 document.getElementById("score-value").innerHTML = score;
-
-                // Showing popup message for a sec
+                // Showing popup message (1 sec)
                 rightAnsPopUp.style.display = "block";
                 wrongAnsPopUp.style.display = "none";
-
                 setTimeout(function(){
                     rightAnsPopUp.style.display = "none";
                 }, 1000);
-
-                // Generate new question
                 generateMCQs();
             }
             // Wrong answer
             else{
-                incorrect.play();
+                incorrect.play(); // Sound
+                // Update score
+                score--;
+                document.getElementById("score-value").innerHTML = score;
+                // Showing popup message (1 sec)
                 wrongAnsPopUp.style.display = "block";
                 rightAnsPopUp.style.display = "none";
                 setTimeout(function(){
@@ -147,24 +141,7 @@ for(var i=1; i<=4; i++){
     };
 }
 
-function gameOver(){
-    isPlay = false;
-    gameOverSound.play();
-    console.log("game over");
-    // Stopping the countdown
-    clearInterval(timeInterval);
-    timerContainer.classList.add("no-show");
-    scoreContainer.classList.add("no-show");
-    gameOverContainer.classList.remove("no-show");
-    body.style.opacity = '0.7';
-    gameOverContainer.style.opacity = "1";
-    document.getElementById("finalScore").innerHTML = score;
-    rightAnsPopUp.style.display = "none";
-    wrongAnsPopUp.style.display = "none";
-
-    document.getElementById("start").innerHTML = "Start";
-}
-
+// Generate answers
 function generateAnswers(no1,no2,typesIndex){
 
     var ans;
@@ -180,7 +157,7 @@ function generateAnswers(no1,no2,typesIndex){
                 break;
         case 3: ans = a * b;
                 break;
-        case 4: ans = (a / b).toFixed(2);
+        case 4: ans = (a / b).toFixed(1);
                 break;
         // case 5: ans = Math.pow(a,b);
         //         break;
@@ -188,65 +165,85 @@ function generateAnswers(no1,no2,typesIndex){
     return ans;
 }
 
+// Generate questions 
 function generateMCQs(){
 
     var answers = [];
     types = ['+','-','*','*','/'];
-
-    // Random numbers between 10 and 20
-    var x = Math.round(9*Math.random()) + 10;
-    // Random numbers between 5 and 15
-    var y = Math.round(9*Math.random()) + 5;
-    
     typesIndex = Math.round((types.length-1)*Math.random());
-    imgIndex = Math.round((images.length-1)*Math.random());
 
+    // Generate question
+    var x = Math.round(9*Math.random()) + 10; // Random numbers between 10 and 20
+    var y = Math.round(9*Math.random()) + 5; // Random numbers between 5 and 15
+    document.getElementById("question").innerHTML = "( " + x + types[typesIndex] + y + " )";
+    
     // For power
-    if(typesIndex == 5){
-        // Random numbers between 5 and 10
-        var w = Math.round(4*Math.random()) + 5;
-        // Random numbers between 1 and 5
-        var z = Math.round(4*Math.random()) + 1;
-        rightAnswer = generateAnswers(rightAnswer1,z,typesIndex);
-    }else{
-        rightAnswer = generateAnswers(x,y,typesIndex);
-    }
+    // if(typesIndex == 5){
+    //     // Random numbers between 5 and 10
+    //     var w = Math.round(4*Math.random()) + 5;
+    //     // Random numbers between 1 and 5
+    //     var z = Math.round(4*Math.random()) + 1;
+    //     rightAnswer = generateAnswers(rightAnswer1,z,typesIndex);
+    // }
+
+    // Assign right answer
+    rightAnswer = generateAnswers(x,y,typesIndex);
     answers.push(rightAnswer)
     
-    img.innerHTML = '<img id="question-img" src="./images/'+images[imgIndex]+'.jpg" width="400" height="200">';
-    
-    document.getElementById("question").innerHTML = "( " + x + types[typesIndex] + y + " )";
     rightIndex = Math.round(3*Math.random()) + 1;
-    
-    // Assign the right answer to one of the options
-    document.getElementById("option" + rightIndex).innerHTML = rightAnswer;
+    document.getElementById("option" + rightIndex).innerHTML = rightAnswer; // Assign the right answer to one of the options
 
-    var i=1;
-    // Assign wrong answer to the remaining options
-    while (i <= totalOptions){
+    // Assign wrong answer
+    for ( var i=1; i <= totalOptions; i++){
 
         if( i != rightIndex){
-            // var w;
-            var wrongAnswer;
-
+            var wrongAnswer, w1, w2;
             do{
-                var w1 = x + Math.round(4*Math.random() + 1);
-                var w2 = y - Math.round(4*Math.random() + 1);
-                console.log("w1: " + w1);
-                console.log("w2: " + w2);
-                console.log("rightAns: " + rightAnswer);
+                if(typesIndex == '0' ){ // Add
+                    w1 = x + Math.round(5*Math.random() + 2);
+                    w2 = y - Math.round(4*Math.random() + 1);
+                }else if(typesIndex == '1' ){ // Subtract
+                    w1 = x + Math.round(4*Math.random() + 2);
+                    w2 = y + Math.round(5*Math.random() + 2);
+                }else if(typesIndex == '2' || typesIndex == '3'){ // Multiply
+                    w1 = x - Math.round(5*Math.random() + 1);
+                    w2 = y + Math.round(3*Math.random() + 2);
+                }else if(typesIndex == '4'){ // Divide
+                    w1 = x + Math.round(3*Math.random() + 1);
+                    w2 = y + Math.round(2*Math.random() + 1);
+                }
                 wrongAnswer = generateAnswers(w1,w2,typesIndex);
-                console.log("wrongAns: " + wrongAnswer);
             }while( answers.indexOf(wrongAnswer) > -1 )
             
-            document.getElementById("option" + i).innerHTML = wrongAnswer;
+            document.getElementById("option" + i).innerHTML = wrongAnswer; // Assign wrong answer to the remaining options
             answers.push(wrongAnswer);
-            console.log(answers);
-            i++;
+            // console.log(answers);
         }else{
-            i++;
             continue;
         }
-        
     }
+
+    // Display a random image
+    imgIndex = Math.round((images.length-1)*Math.random());
+    img.innerHTML = '<img id="question-img" src="./images/'+images[imgIndex]+'.jpg" width="400" height="200">';
+
+}
+
+// Game Over
+function gameOver(){
+
+    console.log("game over");    
+    isPlay = false;
+    gameOverSound.play(); // Sound
+    clearInterval(timeInterval); // Stopping the countdown
+    document.getElementById("finalScore").innerHTML = score;
+    body.style.opacity = '0.7';
+    gameOverContainer.style.opacity = "1";
+    timerContainer.classList.add("no-show");
+    scoreContainer.classList.add("no-show");
+    gameOverContainer.classList.remove("no-show");
+    rightAnsPopUp.style.display = "none";
+    wrongAnsPopUp.style.display = "none";
+    document.getElementById("start").innerHTML = "Start";
+
 }
